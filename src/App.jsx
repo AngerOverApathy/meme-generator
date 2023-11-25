73de9c00ac66db88fec8b1cecdee86171dc18568
AngerOverApathy <violasian@gmail.com>
2023-11-24 20:13:19 -0800

    created banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import './App.css';
+import trollFace from './assets/icons/trollface.png'
 
 export default function App() {
   return (
-    <div className="container">
+
+    <div>
+
       <div className="banner">
+        <img src={trollFace} className='troll' alt="Troll Face" />
+        <h3>MemeGenerator</h3>
+        <p className="course">React Course - Project 3</p>
+      </div>
+
+      <div className="content">
+        <div className="inputs">
+            <input type="text" className="text-box"/>
+            <input type="text" className="text-box"/>
+        </div>
+        <button>Get a new meme image</button>
+      </div>
+
+      <div className="meme"> 
 
       </div>
+
     </div>
 )}
