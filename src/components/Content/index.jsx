6d2582df148699d07abd75ecd8ead9520fd6fea8
AngerOverApathy <violasian@gmail.com>
2023-11-27 16:26:52 -0800

    updated style

--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -1,20 +1,20 @@
-// import React from "react";
+import React from "react";
 
-// export default function Content() {
-//     return (
-//         <>
-//             <div className="content">
-//                 <form className="inputs">
-//                     <input type="text" className="text-box" placeholder='Upper Text'/>
-//                     <input type="text" className="text-box" placeholder='Lower Text'/>
-//                 </form>
-//                 <button className="btn">Get a new meme image  🖼</button>
-//             </div>
+export default function Content() {
+    return (
+        <>
+            <div className="content">
+                <form className="inputs">
+                    <input type="text" className="text-box" placeholder='Upper Text'/>
+                    <input type="text" className="text-box" placeholder='Lower Text'/>
+                </form>
+                <button className="btn">Get a new meme image  🖼</button>
+            </div>
 
-//             <div className="meme"> 
-//                 <p>image placeholder</p>
-//             </div>
-//         </>
-//   )
-// }
+            <div className="meme"> 
+                <p>image placeholder</p>
+            </div>
+        </>
+  )
+}
 
